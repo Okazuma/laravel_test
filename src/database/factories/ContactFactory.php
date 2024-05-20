12c6e0da1a6d4eefc947a6de834f79475939364b
kazuma oshima <kxxxzg@gmail.com>
2024-05-20 13:16:35 +0900

    確認テストの提出

--- src/database/factories/ContactFactory.php
+++ src/database/factories/ContactFactory.php
@@ -20,13 +20,7 @@ class ContactFactory extends Factory
         $categoryId = $this->faker->randomElement($categoryIds);
         $sentence = 'ご対応よろしくお願いいたします。';
 
-        // $firstName = $this->faker->firstName;
-        // $lastName = $this->faker->lastName;
-        // $email = strtolower($lastName.'.'.$firstName) . '@example.com';
 
-        // $category = Category::find($categoryId);
-
-        // $detail = $this->generateInquiryContent($category->id,$category->content);
 
         return [
             //
@@ -34,30 +28,12 @@ class ContactFactory extends Factory
             'first_name' =>$this->faker->firstName(),
             'last_name' =>$this->faker->lastName(),
             'gender' =>$this->faker->randomElement([1,2,3]),
-            'email' =>$this->$faker->safeEmail(),
+            'email' =>$this->faker->safeEmail(),
             'tel' =>$this->faker->phoneNumber(),
             'address' =>$this->faker->address(),
             'detail' =>$sentence,
         ];
     }
-
-    // private function
-    // generateInquiryContent($categoryKey,$categoryName)
-    // {
-    //     switch ($categoryKey){
-    //         case 1:
-    //             return $faker->text(50).''.$categoryName.'の詳細を知りたいです。';
-    //         case 2:
-    //             return $faker->text(50).''.$categoryName.'の手続きを教えてください。';
-    //         case 3:
-    //             return $faker->text(50).''.$categoryName.'がありお問い合わせをしました。';
-    //         case 4:
-    //             return $faker->text(50).''.$categoryName.'を電話で対応して欲しいです。';
-    //         case 5:
-    //             return $faker->text(50).''.$categoryName.'について知りたいです。';
-
-    //     }
-
     }
 
 
